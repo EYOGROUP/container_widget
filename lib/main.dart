9678da0240398c0f09f63a07f71cd37d9782e5dc
Your NameYounes <youmows@gmail.com>
2023-07-07 16:52:18 +0200

    Added Wrap Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+bool onHover = false;
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +25,6 @@ class ContainerApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        
         centerTitle: true,
         elevation: 10,
         leading: IconButton(
@@ -65,6 +65,7 @@ class ContainerApp extends StatelessWidget {
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               margin: const EdgeInsets.only(bottom: 50.0),
@@ -83,79 +84,101 @@ class ContainerApp extends StatelessWidget {
                     fontWeight: FontWeight.bold),
               ),
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(right: 20.0),
-                    height: 100,
-                    width: 150,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: const Text(
-                      "I'm Container",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
+            Center(
+              child: Container(
+                color: Colors.blueAccent,
+                height: 400,
+                width: 250,
+                child: Center(
+                  child: Wrap(
+                    // alignment: WrapAlignment.start,
+                    direction: Axis.vertical,
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 80,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.amber.shade700,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Text(
+                          "1",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 129, 22, 22),
+                              fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        width: 80,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.amber.shade700,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Text(
+                          "2",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        width: 80,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.amber.shade700,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Text(
+                          "3",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        width: 80,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.amber.shade700,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Text(
+                          "4",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        width: 80,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.amber.shade700,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Text(
+                          "5",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        width: 80,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.amber.shade700,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Text(
+                          "6",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(right: 20.0),
-                    height: 100,
-                    width: 150,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: const Text(
-                      "I'm Container",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(right: 20.0),
-                    height: 100,
-                    width: 150,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: const Text(
-                      "I'm Container",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(right: 20.0),
-                    height: 100,
-                    width: 150,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: const Text(
-                      "I'm Container",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
             Container(
@@ -187,5 +210,50 @@ class ContainerApp extends StatelessWidget {
     //     ],
     //   ),
     // ),
+  }
+}
+
+class ButtonHover extends StatefulWidget {
+  const ButtonHover({super.key});
+
+  @override
+  State<ButtonHover> createState() => _ButtonHoverState();
+}
+
+class _ButtonHoverState extends State<ButtonHover>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+          backgroundColor:
+              MaterialStatePropertyAll(onHover ? Colors.amber : Colors.blue),
+          shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+          padding: MaterialStatePropertyAll(EdgeInsets.all(20))),
+      onPressed: () {
+        setState(() {
+          onHover = true;
+        });
+      },
+      child: Text(
+        "Click me!",
+        style: TextStyle(fontSize: 22),
+      ),
+    );
   }
 }
